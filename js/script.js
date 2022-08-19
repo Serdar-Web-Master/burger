b24f1688874adddae9a630b78d6a6d4863db8e11
Serdar <serdarcholukov@yandex.com>
2022-08-19 16:15:17 +0500

    finish

--- js/script.js
+++ js/script.js
@@ -48,6 +48,22 @@ else {
    document.body.classList.add("_pc");
 }
 
+// Меню Бургер
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+   
+   iconMenu.addEventListener("click",function (e) {
+      document.body.classList.toggle("_lock");
+      iconMenu.classList.toggle("_active");
+      menuBody.classList.toggle("_active");
+   }) 
+}
+
+
+
 // Прокрутка При Клики
 
 const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
@@ -61,6 +77,12 @@ if (menuLinks.length>0) {
       if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
          const gotoBlock = document.querySelector(menuLink.dataset.goto);
          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
+
+         if (iconMenu.classList.contains("_active")) {
+            document.body.classList.remove("_lock");
+            iconMenu.classList.remove("_active");
+            menuBody.classList.remove("_active");
+         }
 
          window.scrollTo({
             top: gotoBlockValue,
